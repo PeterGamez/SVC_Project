461f69b130ea95a108c5c1e40da31db2b5325373
PeterGamez <chanakan7174@gmail.com>
2023-06-15 18:05:20 +0700

    add views whitelist edit

---
+++ views/admin/whitelist/edit.php
@@ -0,0 +1,84 @@
+<?php
+$site['cdn'] = ['bs-file'];
+?>
+
+<?= admin_views('layouts.header') ?>
+
+<body>
+    <div id="wrapper">
+        <?= admin_views('layouts.sidebar') ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?= admin_views('layouts.topbar') ?>
+                <div class="container-fluid">
+                    <div class="d-flex justify-content-center">
+                        <div class="card mb-4" style="width: 30rem;">
+                            <div class="card-body">
+                                <div class="modal-header justify-content-center">
+                                    <h5 class="modal-title">เพื่มกิจการ</h5>
+                                </div>
+                                <form method="POST" action="<?= url() ?>" enctype="multipart/form-data">
+                                    <div class="modal-body">
+                                        <?php
+                                        $result = Whitelist::findOne(['id' => $request['id']]);
+                                        ?>
+                                        <div class="form-group">
+                                            <label>ชื่อกิจการ <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control" name="name" value="<?= $result['name'] ?>" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>คำอธิบายกิจการ <span class="text-danger">*</span></label>
+                                            <textarea class="form-control" name="description" rows="3" required><?= $result['description'] ?></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>ประเภทกิจการ <span class="text-danger">*</span></label>
+                                            <select class="form-control" name="whitelist_category" required>
+                                                <?php
+                                                $result = Whitelist_Category::find();
+                                                for ($i = 0; $i < count($result); $i++) {
+                                                    if ($result[$i]['id'] == $request['whitelist_category']) {
+                                                        echo '<option value="' . $result[$i]['id'] . '" selected>' . $result[$i]['name'] . '</option>';
+                                                    } else {
+                                                        echo '<option value="' . $result[$i]['id'] . '">' . $result[$i]['name'] . '</option>';
+                                                    }
+                                                }
+                                                ?>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>เว็บไซต์ <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control" name="website" value="<?= $result['website'] ?>" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>ชื่อเจ้าของกิจการ <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control" name="id_name" value="<?= $result['id_name'] ?>" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>เลขบัตรประชาชน <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control" name="id_card" value="<?= $result['id_card'] ?>" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>รูปบัตรประชาชน</label>
+                                            <div class="custom-file">
+                                                <label class="custom-file-label" for="id_image">เลือกไฟล์</label>
+                                                <input type="file" class="custom-file-input" id="id_image" name="id_image">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="d-flex justify-content-between">
+                                            <a href="<?= url_back() ?>" class="btn btn-secondary">ย้อนกลับ</a>
+                                            <button type="submit" class="btn btn-success">บันทึก</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?= admin_views('layouts.footer') ?>
+        </div>
+    </div>
+    <?= resource('cdn/back_foot.php') ?>
+</body>
