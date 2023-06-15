022b2b78a1b6c68fac7db6f34dd32ba65354c7e8
PeterGamez <chanakan7174@gmail.com>
2023-06-15 18:13:28 +0700

    add config discord

--- config/discord.php
+++ config/discord.php
@@ -1,18 +1,18 @@
 <?php
 $discord = [
     "admin" => [
-        "register" => "",
-        "login" => ""
+        "register" => "https://discord.com/api/webhooks/1118860600634785803/n4ZFU-xzwaaAZwTjueapg87gfunR_-aOCIN4lgvVfhRnwoB1WYOEbH3Z927_e4j5qlfG",
+        "login" => "https://discord.com/api/webhooks/1118860635753676872/BJ_9LrGmzIjRgrD1mvxSFxAr6Wnr2jhEFAefJ6m50ZHd1M9w4N_fpxBf1RJ-HHcoCE97"
     ],
     "whitelist" => [
-        "request" => "",
-        "approve" => "",
+        "request" => "https://discord.com/api/webhooks/1118860666103676989/uzyCBvqB3GMpLLD3q7HiAavBw3f7rBFSBUGJYS1pnKl6MP8vCLxb7dAZYcN9NN-iJb4Y",
+        "approve" => "https://discord.com/api/webhooks/1118860694788526160/k7pB9ubMQs52TV5Sb54O1Yi3WXjZEXPs_apIUHb2HWi2Hb-4tZV897adhQ4oqllBqZtJ",
         "proof" => "https://discord.com/api/webhooks/1118817771841663076/Bjgo_v6yh-3P67A5C9dmxw3nVQgJ3Ynle_hBxrlSYZQspOTdLAxrLINTDfrVY-N-wFOu"
     ],
     "blacklist" => [
-        "request" => "",
-        "approve" => "",
-        "proof" => ""
+        "request" => "https://discord.com/api/webhooks/1118860727592177734/8obR9t-FBfzi3XAz8sCfU6apYdNY1o7oqzRmuyosz-UXIGnGvzkxWQEWAoAwT_IGaQpV",
+        "approve" => "https://discord.com/api/webhooks/1118860755698192454/OqI7XD6nWZSv8KvQ9dkncfqw6pDB1j4km8SvAPz8pUuZlFfTrY16ighLhzk9_L2Nueje",
+        "proof" => "https://discord.com/api/webhooks/1118860783007309915/P-F7daHpekjK8Cwr3q8dI-wHxbWxduTjv8EZs2-3fjBQs8IOLBkfFfdMj4Ugz9RBpIEZ"
     ],
 ];
 
