d9f5fc415838e28bd7a3e99cfe1c6c34538f3c88
PeterGamez <chanakan7174@gmail.com>
2023-06-17 18:59:16 +0700

    add post method

--- app/class/Discord.php
+++ app/class/Discord.php
@@ -1,12 +1,25 @@
 <?php
 class Discord
 {
+    public static function post($url, $data)
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($response);
+    }
+    
     public static function postImage($url, $data)
     {
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($curl);
         curl_close($curl);
