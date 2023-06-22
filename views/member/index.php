bce5b7381cd54fb202e6f49e5b8a60c1f19ef3fb
PeterGamez <chanakan7174@gmail.com>
2023-06-22 17:23:33 +0700

    commit

--- views/member/index.php
+++ views/member/index.php
@@ -10,9 +10,8 @@
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
                     </div>
-                    <strong>ยินดีต้อนรับ!</strong> คุณ <?= $_SESSION['user_username'] ?> สวัสดีครับ ยินดีต้อนรับสู่ <?= config('site.name') ?> ครับ
-                    <?= views('layouts.back_footer') ?>
                 </div>
+                <?= views('layouts.back_footer') ?>
             </div>
         </div>
     </div>
