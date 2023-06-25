ddff8c0615a4698b2de0e503ab84081e811ba166
PeterGamez <chanakan7174@gmail.com>
2023-06-25 10:44:49 +0700

    commit

--- resource/cdn/back_foot.php
+++ resource/cdn/back_foot.php
@@ -20,6 +20,6 @@
 <?php if (isset($site['cdn']) and in_array('datatables', $site['cdn'])) { ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net/1.13.4/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-bs4/1.13.4/dataTables.bootstrap4.min.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-responsive/2.4.1/dataTables.responsive.min.js"></script> 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-responsive-bs4/2.4.1/responsive.bootstrap4.min.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-responsive/2.4.1/dataTables.responsive.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-responsive-bs4/2.4.1/responsive.bootstrap4.min.js"></script>
 <?php } ?>
