7e8e517280cc4270b9c655080f2d021865a8dcef
PeterGamez <chanakan7174@gmail.com>
2023-08-08 21:04:22 +0700

    bootstrap.bundle.js to bootstrap.js

--- views/template/back/cdn_footer.php
+++ views/template/back/cdn_footer.php
@@ -1,7 +1,7 @@
 <!-- Jquery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.0/js/bootstrap.min.js"></script>
 <!-- Bootstrap file input -->
 <?php if (isset($site['cdn']) and in_array('bs-file', $site['cdn'])) {
 ?>
