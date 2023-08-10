4b8c453f7cdc885f77eadceb0e0a3239ad86f6a0
PeterGamez <chanakan7174@gmail.com>
2023-08-10 10:09:47 +0700

    add blank page

---
+++ views/member/blank.php
@@ -0,0 +1,17 @@
+<?= views('template/back/header') ?>
+
+<body>
+    <div id="wrapper">
+        <?= member_views('layouts.sidebar') ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?= member_views('layouts.topbar') ?>
+                <div class="container-fluid">
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                </div>
+            </div>
+           <?= views('template/back/footer') ?>
+        </div>
+    </div>
+    <?= views('template/back/cdn_footer') ?>
+</body>
