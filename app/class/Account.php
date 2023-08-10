ef48b24e8c811788e6fb25b76b6d6921b5d73cd9
PeterGamez <chanakan7174@gmail.com>
2023-08-10 10:07:01 +0700

    add email in session

--- app/class/Account.php
+++ app/class/Account.php
@@ -12,6 +12,7 @@ class Account
         $_SESSION['login'] = true;
         $_SESSION['user_id'] = $data['id'];
         $_SESSION['user_username'] = $data['username'];
+        $_SESSION['user_email'] = $data['email'];
         $_SESSION['user_avatar'] = isset($data['avatar']) ? $data['avatar'] : resource('images/logo-1000.png', true);
         $_SESSION['user_role'] = $data['role'];
     }
