deaafad08ef4d6286904a248d63fe74ba22fff58
PeterGamez <chanakan7174@gmail.com>
2023-08-11 10:59:41 +0700

    fix views

--- views/member/404.php
+++ views/member/404.php
@@ -5,10 +5,10 @@ $site['cdn'] = ['404'];
 
 <body>
     <div id="wrapper">
-        <?= admin_views('layouts.sidebar') ?>
+        <?= member_views('layouts.sidebar') ?>
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
-                <?= admin_views('layouts.topbar') ?>
+                <?= member_views('layouts.topbar') ?>
                 <div class="container-fluid">
                     <div class="text-center">
                         <div class="error mx-auto" data-text="404">404</div>
