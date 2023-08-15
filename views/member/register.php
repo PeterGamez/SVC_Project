f4beeb8d105e7ab4c293f9e7a59355a6ce0aa8b1
PeterGamez <chanakan7174@gmail.com>
2023-08-15 14:46:24 +0700

    fix bug

--- views/member/register.php
+++ views/member/register.php
@@ -14,19 +14,23 @@ $site['cdn'] = ['login'];
                             <h1 class="h4 text-gray-900 mb-4">Register</h1>
                         </div>
                         <form class="user" method="POST" action="<?= url() ?>">
-                            <div class="form-group">
+                            <div class="mb-3">
                                 <input type="text" class="form-control form-control-user" name="user" placeholder="Username" required pattern="a-zA-Z0-9_" minlength="5" maxlength="20">
                             </div>
-                            <div class="form-group">
+                            <div class="mb-3">
                                 <input type="email" class="form-control form-control-user" name="email" placeholder="Email" required>
                             </div>
-                            <div class="form-group">
+                            <div class="mb-3">
                                 <input type="password" class="form-control form-control-user" name="password1" placeholder="Password" required minlength="8">
                             </div>
-                            <div class="form-group">
+                            <div class="mb-3">
                                 <input type="password" class="form-control form-control-user" name="password2" placeholder="Confirm Password" required minlength="8">
                             </div>
-                            <div class="form-group d-flex justify-content-center">
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" name="agree" id="agree" required>
+                                <label class="form-check-label" for="agree">I agree to the <a href="<?= url('tos') ?>">Terms of Service</a></label>
+                            </div>
+                            <div class="mb-3 d-flex justify-content-center">
                                 <div class="cf-turnstile" data-sitekey="<?= config('site.cloudflare.turnstile.key') ?>"></div>
                             </div>
                             <button type="submit" class="btn btn-primary btn-user btn-block"> Register </button>
